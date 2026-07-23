4b2513de3258c6209ec52fbb0034a2313b84dbd7
Hatim Kagalwala <hjk612@nyu.edu>
2026-07-22 17:04:35 -0700

    Step 4: decode_ids_to_tokens

--- model.py
+++ model.py
@@ -41,7 +41,6 @@ def encode_sentence_to_ids(sentence, token_to_id, unk_token='<unk>'):
 
 # Step 4 - decode_ids_to_tokens
 def decode_ids_to_tokens(ids, id_to_token):
-    # TODO: map each id in ids to its token string via id_to_token and return the list
     return [id_to_token[_] for _ in ids]
 
 # Step 5 - pad_id_sequence
